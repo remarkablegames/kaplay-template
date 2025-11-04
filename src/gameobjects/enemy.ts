db4078d5e3367d56d0c79b0cc4cb346a66caf2ee
Mark <remarkablemark@users.noreply.github.com>
2025-11-03 23:05:08 -0500

    refactor(gameobjects): tidy enemy

--- src/gameobjects/enemy.ts
+++ src/gameobjects/enemy.ts
@@ -1,5 +1,12 @@
 import { Sprite, Tag } from '../constants'
 
 export function addEnemy(x: number, y: number) {
-  return add([sprite(Sprite.Ghosty), pos(x, y), anchor('center'), Tag.Enemy])
+  const enemy = add([
+    sprite(Sprite.Ghosty),
+    pos(x, y),
+    anchor('center'),
+    Tag.Enemy,
+  ])
+
+  return enemy
 }
